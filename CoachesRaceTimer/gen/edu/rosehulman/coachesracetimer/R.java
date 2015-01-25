9bad6ca5d686bc9f923bd955addb6d3b29821b38
ALEX LEVERSEN <leversad@rose-hulman.edu>
2015-01-24 19:48:54 -0500

    Added convert activity
    Minor commit, just to get files added.

--- CoachesRaceTimer/gen/edu/rosehulman/coachesracetimer/R.java
+++ CoachesRaceTimer/gen/edu/rosehulman/coachesracetimer/R.java
@@ -14,11 +14,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_convert=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int event_1_string=0x7f040002;
+        public static final int event_2_string=0x7f040003;
         public static final int hello_world=0x7f040001;
+        public static final int title_activity_convert=0x7f040004;
     }
     public static final class style {
         /** 
