e9e36beecef56e6dc5d38fb1e24e5b121f5dc6ad
ALEX LEVERSEN <leversad@rose-hulman.edu>
2015-01-26 00:01:42 -0500

    ConvertView complete
    ConvertView is complete in terms of look, at least for now. Includes two
    side-by-side views of all events.

--- CoachesRaceTimer/gen/edu/rosehulman/coachesracetimer/R.java
+++ CoachesRaceTimer/gen/edu/rosehulman/coachesracetimer/R.java
@@ -8,30 +8,43 @@
 package edu.rosehulman.coachesracetimer;
 
 public final class R {
+    public static final class array {
+        public static final int events=0x7f040000;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int Button01=0x7f060006;
-        public static final int button1=0x7f060003;
-        public static final int button2=0x7f060002;
-        public static final int button3=0x7f060001;
-        public static final int button4=0x7f060000;
-        public static final int button5=0x7f060005;
-        public static final int textView1=0x7f060004;
+        public static final int Button01=0x7f07000c;
+        public static final int button1=0x7f070009;
+        public static final int button2=0x7f070008;
+        public static final int button3=0x7f070007;
+        public static final int button4=0x7f070006;
+        public static final int button5=0x7f07000b;
+        public static final int event1=0x7f070000;
+        public static final int event2=0x7f070003;
+        public static final int listView1=0x7f070001;
+        public static final int listView2=0x7f070004;
+        public static final int textView1=0x7f07000a;
+        public static final int time1=0x7f070002;
+        public static final int time2=0x7f070005;
     }
     public static final class layout {
         public static final int activity_convert=0x7f030000;
         public static final int activity_main=0x7f030001;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int event_1_string=0x7f040002;
-        public static final int event_2_string=0x7f040003;
-        public static final int hello_world=0x7f040001;
-        public static final int title_activity_convert=0x7f040004;
+        public static final int app_name=0x7f050000;
+        public static final int convert_text=0x7f050008;
+        public static final int default_time=0x7f050007;
+        public static final int event1_text=0x7f050005;
+        public static final int event2_text=0x7f050006;
+        public static final int event_1_string=0x7f050002;
+        public static final int event_2_string=0x7f050003;
+        public static final int hello_world=0x7f050001;
+        public static final int title_activity_convert=0x7f050004;
     }
     public static final class style {
         /** 
@@ -55,10 +68,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
--- CoachesRaceTimer/src/edu/rosehulman/coachesracetimer/ConvertActivity.java
+++ CoachesRaceTimer/src/edu/rosehulman/coachesracetimer/ConvertActivity.java
@@ -4,12 +4,26 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class ConvertActivity extends Activity {
+	
+	ListView event1;
+	ListView event2;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_convert);
+		event1=(ListView) findViewById(R.id.listView1);
+		event2=(ListView) findViewById(R.id.listView2);
+		String[] events = getResources().getStringArray(R.array.events);
+		ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,events);
+		ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,events);
+		event1.setAdapter(adapter1);
+		event2.setAdapter(adapter2);
 	}
 }
