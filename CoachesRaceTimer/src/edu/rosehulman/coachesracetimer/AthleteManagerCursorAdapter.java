e195a5c4b662a9733d792b9763fab40cb51ab17a
ALEX LEVERSEN <leversad@rose-hulman.edu>
2015-02-19 02:29:51 -0500

    Final commit for Thursday
    Final commit

---
+++ CoachesRaceTimer/src/edu/rosehulman/coachesracetimer/AthleteManagerCursorAdapter.java
@@ -0,0 +1,35 @@
+package edu.rosehulman.coachesracetimer;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SimpleCursorAdapter;
+
+public class AthleteManagerCursorAdapter extends SimpleCursorAdapter {
+	
+	AthletesView context;
+
+	public AthleteManagerCursorAdapter(Context context, int layout, Cursor c,
+			String[] from, int[] to, int flags) {
+		super(context, layout, c, from, to, flags);
+		this.context=(AthletesView)context;
+	}
+	
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		final int fPosition = position;
+		final View view = super.getView(position, convertView, parent);
+		view.setOnLongClickListener(new View.OnLongClickListener() {
+			
+			@Override
+			public boolean onLongClick(View v) {
+				AthleteManagerCursorAdapter.this.context.removeAthlete(fPosition);
+				return true;
+			}
+		});
+		return view;
+		
+	}
+
+}
--- CoachesRaceTimer/src/edu/rosehulman/coachesracetimer/MainActivity.java
+++ CoachesRaceTimer/src/edu/rosehulman/coachesracetimer/MainActivity.java
@@ -1,6 +1,9 @@
 package edu.rosehulman.coachesracetimer;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -14,6 +17,7 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -26,15 +30,17 @@ public class MainActivity extends Activity implements OnClickListener {
 	public static final String KEY_MAIN_EVENT_STRING = "KEY_MAIN_EVENT_STRING";
 	public static final String KEY_PR_STRING = "KEY_PR_STRING";
 	private AthleteDataAdapter mAthleteDataAdapter;
-	private SimpleCursorAdapter mCursorAdapter;
+	private AthleteTimeCursorAdapter mCursorAdapter;
 
 	StopWatch stopWatch;
 	Button startButton;
 	Button stopButton;
 	Button addAthleteButton;
+	Button saveRaceButton;
 	TextView timeView;
 	TimerHandler timerHandler;
 	ListView athleteList;
+	Map<String,List<String>> lapMap;
 //	ArrayList<String> athleteArray;
 //	ArrayAdapter<String> adapter;
 
@@ -43,13 +49,16 @@ public class MainActivity extends Activity implements OnClickListener {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		lapMap = new HashMap<String,List<String>>();
 		timeView = (TextView) findViewById(R.id.textView1);
 		startButton = (Button) findViewById(R.id.StartButton);
 		stopButton = (Button) findViewById(R.id.StopButton);
 		addAthleteButton = (Button) findViewById(R.id.addAthButton);
+		saveRaceButton = (Button) findViewById(R.id.saveButton);
 		startButton.setOnClickListener(this);
 		stopButton.setOnClickListener(this);
 		addAthleteButton.setOnClickListener(this);
+		saveRaceButton.setOnClickListener(this);
 		stopWatch = new StopWatch();
 		timerHandler = new TimerHandler(stopWatch, timeView);
 		athleteList = (ListView) findViewById(R.id.athleteList);
@@ -59,8 +68,12 @@ public class MainActivity extends Activity implements OnClickListener {
 		Cursor cursor = mAthleteDataAdapter.getAthletesCursor();
 		String[] fromColumns = new String[] {AthleteDataAdapter.KEY_FIRST_NAME,AthleteDataAdapter.KEY_LAST_NAME};
 		int[] toTextViews = new int[]{R.id.athleteFirstNameText,R.id.athleteLastNameText};
-		mCursorAdapter = new SimpleCursorAdapter(this,R.layout.athlete_view,cursor,fromColumns,toTextViews,0);
+		mCursorAdapter = new AthleteTimeCursorAdapter(this,R.layout.athlete_view,cursor,fromColumns,toTextViews,0);
 		athleteList.setAdapter(mCursorAdapter);
+//		for(int i=0;i<mCursorAdapter.getCursor().getCount();i++){
+//			lapMap.put(mCursorAdapter.getCursor().getString(1)+" "+mCursorAdapter.getCursor().getString(2), new ArrayList<String>());
+//		}TODO: FIX THIS!!!!!!
+		Log.d(CRT,lapMap.toString());
 		
 //		athleteArray = new ArrayList<String>();
 //		for(int i=1;i<15;i++){
@@ -76,14 +89,23 @@ public class MainActivity extends Activity implements OnClickListener {
 		if (startButton == v) {
 			Log.d(CRT, "Start");
 			this.timerHandler.sendEmptyMessage(TimerHandler.TIMER_START);
+//			mCursorAdapter.setLapEnabled(true);
+//			mCursorAdapter.setStopEnabled(true);
 		} else if (stopButton == v) {
 			Log.d(CRT, "Stop");
+//			mCursorAdapter.setStopEnabled(false);
+//			mCursorAdapter.setLapEnabled(false);
 			timerHandler.sendEmptyMessage(TimerHandler.TIMER_STOP);
 		} else if (addAthleteButton == v) {
 			Log.d(CRT, "addAthleteButton");
+			if(!stopWatch.isAlive()){//Can't add athletes once the stopwatch has started
 			Intent newAthleteIntent = new Intent(this, NewAthleteActivity.class);
 			newAthleteIntent.putExtra(AthletesView.EDIT_OR_NEW, AthletesView.NEW);
 			startActivityForResult(newAthleteIntent, REQUEST_CODE_NEW_ATHLETE);
+			}
+		}else if(saveRaceButton == v){
+			Toast.makeText(MainActivity.this, "Saving...", Toast.LENGTH_LONG).show();
+			Toast.makeText(MainActivity.this, "Saved!", Toast.LENGTH_SHORT).show();
 		}
 		Log.d(CRT, "TEST");
 	}
@@ -96,7 +118,7 @@ public class MainActivity extends Activity implements OnClickListener {
 				Log.d(CRT, "Result ok!");
 				Athlete a = new Athlete();
 				
-//				a.setRank(data.getIntExtra(KEY_RANK_STRING,0)); //TODO: make this work. Quick.
+//				a.setRank(data.getIntExtra(KEY_RANK_STRING,0)); 
 //				a.setName(data.getStringExtra(KEY_NAME_STRING));
 				a.setFirstName(data.getStringExtra(KEY_FIRST_NAME_STRING));
 				a.setLastName(data.getStringExtra(KEY_LAST_NAME_STRING));
@@ -105,6 +127,10 @@ public class MainActivity extends Activity implements OnClickListener {
 				mAthleteDataAdapter.addAthlete(a);
 				Cursor cursor = mAthleteDataAdapter.getAthletesCursor();
 				mCursorAdapter.changeCursor(cursor);
+				lapMap = new HashMap<String,List<String>>();
+//				for(int i=0;i<mCursorAdapter.getCursor().getCount();i++){
+//					lapMap.put(mCursorAdapter.getCursor().getString(0)+" "+mCursorAdapter.getCursor().getString(1), new ArrayList<String>());
+//				}TODO: FIX THIS!!!!!!
 //				int size = athleteArray.size();
 //				int rank = data.getIntExtra(KEY_RANK_STRING,
 //						size);
@@ -152,20 +178,39 @@ public class MainActivity extends Activity implements OnClickListener {
 		return false;
 	}
 	
-	private Athlete getAthlete(long id){
-		return mAthleteDataAdapter.getAthlete(id);
+//	private Athlete getAthlete(long id){
+//		return mAthleteDataAdapter.getAthlete(id);
+//	}
+//	
+//	private void editAthlete(Athlete a){
+//		mAthleteDataAdapter.updateAthlete(a);
+//		Cursor cursor = mAthleteDataAdapter.getAthletesCursor();
+//		mCursorAdapter.changeCursor(cursor);
+//	}
+//	
+//	private void removeAthlete(long id){
+//		mAthleteDataAdapter.removeAthlete(id);
+//		Cursor cursor = mAthleteDataAdapter.getAthletesCursor();
+//		mCursorAdapter.changeCursor(cursor);
+//	}
+
+	public void stop(View v) {
+		lap(v);
+		Button button = (Button)v.findViewById(R.id.lapButton);
+//		if(button!=null){
+//			button.setEnabled(false);
+//		}
 	}
-	
-	private void editAthlete(Athlete a){
-		mAthleteDataAdapter.updateAthlete(a);
-		Cursor cursor = mAthleteDataAdapter.getAthletesCursor();
-		mCursorAdapter.changeCursor(cursor);
-	}
-	
-	private void removeAthlete(long id){
-		mAthleteDataAdapter.removeAthlete(id);
-		Cursor cursor = mAthleteDataAdapter.getAthletesCursor();
-		mCursorAdapter.changeCursor(cursor);
+
+	public void lap(View v) {
+		Log.d(CRT,"Lap called");
+		TextView first = (TextView)v.findViewById(R.id.athleteFirstNameText);
+		if(first!=null){
+			String firstName = first.getText().toString();
+			String lastName = ((TextView)v.findViewById(R.id.athleteLastNameText)).getText().toString();
+//			lapMap.get(firstName+" "+lastName).add(stopWatch.getTime());TODO: FIX THIS!!!!!!
+			Log.d(CRT,lapMap.toString());
+		}
 	}
 	
 }
